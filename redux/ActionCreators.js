8e8229626c77b1b2f0cda32ddba12b7a900cd4b4
Sam Amos <sma.amos@gmail.com>
2020-02-19 21:18:02 -0500

    favorites delete button

--- redux/ActionCreators.js
+++ redux/ActionCreators.js
@@ -153,6 +153,10 @@ export const addFavorite = campsiteId => ({
     payload: campsiteId
 });
 
+export const deleteFavorite = campsiteId => ({
+    type: ActionTypes.DELETE_FAVORITE,
+    payload: campsiteId
+}); 
 
 export const postComment = (campsiteId, rating, author, text) => dispatch => {
     const newComment = {
