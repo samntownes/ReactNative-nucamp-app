32a73a90c6f999e9308447bf18079ce40ac0c456
Sam Amos <sma.amos@gmail.com>
2020-02-22 11:14:00 -0500

    deleted modal css

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -133,22 +133,6 @@ const styles = StyleSheet.create({
     },
     formItem: {
         flex: 1
-    },
-    modal: { 
-        justifyContent: 'center',
-        margin: 20
-    },
-    modalTitle: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        backgroundColor: '#5637DD',
-        textAlign: 'center',
-        color: '#fff',
-        marginBottom: 20
-    },
-    modalText: {
-        fontSize: 18,
-        margin: 10
     }
 });
 
